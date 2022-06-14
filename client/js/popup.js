dbbf217aa2e2bb4e86292a69754eea54cc509e93
bhavyakukkar <nigelwithrow78@gmail.com>
2022-06-15 01:33:53 +0530

    client working contacts + messages

--- client/js/popup.js
+++ client/js/popup.js
@@ -1,37 +1,13 @@
 var username = "pranav";
 var password_hash = "a12619925a8e16d9a7a827d8ce6375aa";
-var contact = "bhavya";
 
-function init() {
-    //updateContacts();
-    setInterval(function() {
-        retrieveMessages();
-    }, 500);
-}
-
-function retrieveMessages() {
-
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var parser = new DOMParser();
-            var responseDom = parser.parseFromString(this.responseText, "text/html");
-            var retrievedMessages = responseDom.getElementById('messages');
-            
-            if(foundUpdations(retrievedMessages.childElementCount))
-                updateMessages(retrievedMessages);
-        }
-    };
-
-    var urlParameters = "for="+username+"&between="+contact+"&key="+password_hash;
-    var url = "https://ghost-in-the-heap.000webhostapp.com/Messenger/server/php/fetch_messages.php?"+urlParameters;
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
+var welcomeBoardOpen = true;
+var currentContactOpen = "";
 
 
-function foundUpdations(retrievedMessagesCount) {
-    var existingMessagesCount = document.getElementById('messages').childElementCount;
+//Detect change in existing and retrieved DOM
+function foundUpdations(retrievedMessagesCount, elementId) {
+    var existingMessagesCount = document.getElementById(elementId).childElementCount;
     
     if(retrievedMessagesCount == existingMessagesCount)
         return false;
@@ -40,12 +16,110 @@ function foundUpdations(retrievedMessagesCount) {
 }
 
 
+
+//Main method
+function init() {
+    
+    //Retrieve All Contacts & Add Buttons
+    setInterval(function() {
+        retrieveContacts();
+    }, 2000);
+
+    /*setInterval(function() {
+        retrieveMessages();
+    }, 500);*/
+}
+
+
+
+//Retrieve & Update Contacts
+function retrieveContacts() {
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var parser = new DOMParser();
+            var responseDom = parser.parseFromString(this.responseText, "text/html");
+            var retrievedContacts = responseDom.getElementById('contacts-list');
+            
+            if(foundUpdations(retrievedContacts.childElementCount, "contacts-list"))
+                updateContacts(retrievedContacts);
+        }
+    };
+
+    var urlParameters = "of="+username+"&key="+password_hash;
+    var url = "https://ghost-in-the-heap.000webhostapp.com/Messenger/server/php/fetch_contacts.php?"+urlParameters;
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function updateContacts(retrievedContacts) {
+
+    var existingContacts = document.getElementById('contacts-list');
+    var contacts = document.getElementById('contacts');
+
+    contacts.removeChild(existingContacts);
+    contacts.appendChild(retrievedContacts);
+
+    addContactButtons(retrievedContacts);
+}
+
+
+
+//Add Event Listeners to Contact Buttons
+function addContactButtons(retrievedContacts) {
+
+    retrievedContacts.childNodes.forEach(contact => {
+        contact.addEventListener("click", function() {
+            
+            currentContactOpen = contact.innerText;
+            if(welcomeBoardOpen) {
+
+                setInterval(function() {
+                    retrieveMessages();
+                }, 300);
+                welcomeBoardOpen = false;
+            }
+            setTimeout(function() {
+                updateContactName();
+            }, 200);
+        });
+    });
+}
+
+function updateContactName() {
+    document.getElementById("contact-name").innerText = currentContactOpen;
+}
+
+
+
+//Retrieve & Update Messages for selected Contact
+function retrieveMessages() {
+    
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var parser = new DOMParser();
+            var responseDom = parser.parseFromString(this.responseText, "text/html");
+            var retrievedMessages = responseDom.getElementById('messages');
+            
+            if(foundUpdations(retrievedMessages.childElementCount, "messages"))
+                updateMessages(retrievedMessages);
+        }
+    };
+
+    var urlParameters = "for="+username+"&between="+currentContactOpen+"&key="+password_hash;
+    var url = "https://ghost-in-the-heap.000webhostapp.com/Messenger/server/php/fetch_messages.php?"+urlParameters;
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
 function updateMessages(retrievedMessages) {
     
-    var messages = document.getElementById('messages');
+    var existingMessages = document.getElementById('messages');
     var chat = document.getElementById('chat');
 
-    chat.removeChild(messages);
+    chat.removeChild(existingMessages);
     chat.insertBefore(retrievedMessages, document.getElementById("new-message"));
     retrievedMessages.children[retrievedMessages.childElementCount-1].scrollIntoView();
 }
